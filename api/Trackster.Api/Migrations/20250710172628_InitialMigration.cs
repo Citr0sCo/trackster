9b4f195d046d039b5e0057db885faee12a723fcc
Milosz Dura <miloszdura@gmail.com>
2025-07-10 18:26:56 +0100

    creating initial migration

---
+++ api/Trackster.Api/Migrations/20250710172628_InitialMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trackster.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Identifier = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Identifier);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TvShowRecord",
+                columns: table => new
+                {
+                    Identifier = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TvShowRecord", x => x.Identifier);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "TvShowRecord");
+        }
+    }
+}
