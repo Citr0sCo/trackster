34528812cca3377458f3946b90e5f3cd6568fd9d
Milosz Dura <miloszdura@gmail.com>
2025-07-11 23:01:00 +0100

    Update WebhookController.cs (#22)

--- api/Trackster.Api/Features/Webhook/WebhookController.cs
+++ api/Trackster.Api/Features/Webhook/WebhookController.cs
@@ -29,15 +29,29 @@ public class WebhookController : ControllerBase
 
             var parsed = HttpUtility.ParseQueryString(payloadString);
 
+             var keyValuePairs = payloadString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+             var dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+             foreach (var pair in keyValuePairs)
+             {
+                 var separatorIndex = pair.IndexOf('=');
+                 if (separatorIndex > 0 && separatorIndex < pair.Length - 1)
+                 {
+                     var key = pair.Substring(0, separatorIndex);
+                     var value = pair.Substring(separatorIndex + 1);
+                     dict[key] = Uri.UnescapeDataString(value);
+                 }
+             }
+
             var webhookRequest = new PlexWebhookRequest
             {
-                @event = parsed["event"],
-                user = bool.TryParse(parsed["user"], out var user) && user,
-                owner = bool.TryParse(parsed["owner"], out var owner) && owner,
-                Account = string.IsNullOrWhiteSpace(parsed["Account"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexAccount>(parsed["Account"]),
-                Server = string.IsNullOrWhiteSpace(parsed["Server"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexServer>(parsed["Server"]),
-                Player = string.IsNullOrWhiteSpace(parsed["Player"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexPlayer>(parsed["Player"]),
-                Metadata = string.IsNullOrWhiteSpace(parsed["Metadata"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexMetadata>(parsed["Metadata"])
+                @event = dict["event"],
+                user = bool.TryParse(dict["user"], out var user) && user,
+                owner = bool.TryParse(dict["owner"], out var owner) && owner,
+                Account = string.IsNullOrWhiteSpace(dict["Account"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexAccount>(dict["Account"]),
+                Server = string.IsNullOrWhiteSpace(dict["Server"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexServer>(dict["Server"]),
+                Player = string.IsNullOrWhiteSpace(dict["Player"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexPlayer>(dict["Player"]),
+                Metadata = string.IsNullOrWhiteSpace(dict["Metadata"]) ? null : JsonConvert.DeserializeObject<PlexWebhookRequest.PlexMetadata>(dict["Metadata"])
             };
 
             _service.HandlePlexWebhook(webhookRequest);
