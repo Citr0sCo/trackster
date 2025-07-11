a5fc50da4c238caa233493d41cf45850155c762c
Milosz Dura <miloszdura@gmail.com>
2025-07-11 19:34:59 +0100

    test

--- api/Trackster.Api/Features/Webhook/PlexWebhookService.cs
+++ api/Trackster.Api/Features/Webhook/PlexWebhookService.cs
@@ -1,19 +1,14 @@
+using Newtonsoft.Json;
 using Trackster.Api.Features.Webhook.Types;
 
 namespace Trackster.Api.Features.Webhook;
 
 public class PlexWebhookService
 {
-    public void HandlePlexWebhook(string? eventName, string? user, string? owner, PlexWebhookRequest.PlexAccount? account, PlexWebhookRequest.PlexServer? server, PlexWebhookRequest.PlexPlayer? player, PlexWebhookRequest.PlexMetadata? metadata)
+    public void HandlePlexWebhook(PlexWebhookRequest? parsedJson)
     {
         Console.WriteLine("--- Plex Webhook Parse Start ---");
-        Console.WriteLine(eventName);
-        Console.WriteLine(user);
-        Console.WriteLine(owner);
-        Console.WriteLine(account);
-        Console.WriteLine(server);
-        Console.WriteLine(player);
-        Console.WriteLine(metadata);
+        Console.WriteLine(JsonConvert.SerializeObject(parsedJson));
         Console.WriteLine("--- Plex Webhook Parse End ---");
     }
 }
