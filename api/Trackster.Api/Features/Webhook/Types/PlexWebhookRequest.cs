f9ce2ad4d269e1b7bb02498f1bc5ee9139894a96
Milosz Dura <miloszdura@gmail.com>
2025-07-12 11:48:09 +0100

    Update PlexWebhookRequest.cs (#29)

--- api/Trackster.Api/Features/Webhook/Types/PlexWebhookRequest.cs
+++ api/Trackster.Api/Features/Webhook/Types/PlexWebhookRequest.cs
@@ -1,62 +1,298 @@
+using Newtonsoft.Json;
+
 namespace Trackster.Api.Features.Webhook.Types;
 
 public class PlexWebhookRequest
 {
-    public string @event { get; set; }
-    public bool user { get; set; }
-    public bool owner { get; set; }
-    public PlexAccount Account { get; set; }
-    public PlexServer Server { get; set; }
-    public PlexPlayer Player { get; set; }
-    public PlexMetadata Metadata { get; set; }
+    [JsonProperty("event")]
+    public string Event { get; set; }
 
-    public class PlexAccount
-    {
-        public int id { get; set; }
-        public string thumb { get; set; }
-        public string title { get; set; }
-    }
+    [JsonProperty("user")]
+    public bool User { get; set; }
 
-    public class PlexMetadata
-    {
-        public string librarySectionType { get; set; }
-        public string ratingKey { get; set; }
-        public string key { get; set; }
-        public string parentRatingKey { get; set; }
-        public string grandparentRatingKey { get; set; }
-        public string guid { get; set; }
-        public int librarySectionID { get; set; }
-        public string type { get; set; }
-        public string title { get; set; }
-        public string grandparentKey { get; set; }
-        public string parentKey { get; set; }
-        public string grandparentTitle { get; set; }
-        public string parentTitle { get; set; }
-        public string summary { get; set; }
-        public int index { get; set; }
-        public int parentIndex { get; set; }
-        public int ratingCount { get; set; }
-        public string thumb { get; set; }
-        public string art { get; set; }
-        public string parentThumb { get; set; }
-        public string grandparentThumb { get; set; }
-        public string grandparentArt { get; set; }
-        public int addedAt { get; set; }
-        public int updatedAt { get; set; }
-    }
+    [JsonProperty("owner")]
+    public bool Owner { get; set; }
 
-    public class PlexPlayer
-    {
-        public bool local { get; set; }
-        public string publicAddress { get; set; }
-        public string title { get; set; }
-        public string uuid { get; set; }
-    }
+    [JsonProperty("Account")]
+    public Account Account { get; set; }
 
-    public class PlexServer
-    {
-        public string title { get; set; }
-        public string uuid { get; set; }
-    }
+    [JsonProperty("Server")]
+    public Server Server { get; set; }
+
+    [JsonProperty("Player")]
+    public Player Player { get; set; }
+
+    [JsonProperty("Metadata")]
+    public Metadata Metadata { get; set; }
+}
+
+public class Account
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("thumb")]
+    public string Thumb { get; set; }
+
+    [JsonProperty("title")]
+    public string Title { get; set; }
+}
+
+public class Director
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("filter")]
+    public string Filter { get; set; }
+
+    [JsonProperty("tag")]
+    public string Tag { get; set; }
+
+    [JsonProperty("tagKey")]
+    public string TagKey { get; set; }
+}
+
+public class Guid
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+}
+
+public class Image
+{
+    [JsonProperty("alt")]
+    public string Alt { get; set; }
+
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+}
+
+public class Metadata
+{
+    [JsonProperty("librarySectionType")]
+    public string LibrarySectionType { get; set; }
+
+    [JsonProperty("ratingKey")]
+    public string RatingKey { get; set; }
+
+    [JsonProperty("key")]
+    public string Key { get; set; }
+
+    [JsonProperty("parentRatingKey")]
+    public string ParentRatingKey { get; set; }
+
+    [JsonProperty("grandparentRatingKey")]
+    public string GrandparentRatingKey { get; set; }
+
+    [JsonProperty("guid")]
+    public string Reference { get; set; }
+
+    [JsonProperty("parentGuid")]
+    public string ParentGuid { get; set; }
+
+    [JsonProperty("grandparentGuid")]
+    public string GrandparentGuid { get; set; }
+
+    [JsonProperty("grandparentSlug")]
+    public string GrandparentSlug { get; set; }
+
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("title")]
+    public string Title { get; set; }
+
+    [JsonProperty("grandparentKey")]
+    public string GrandparentKey { get; set; }
+
+    [JsonProperty("parentKey")]
+    public string ParentKey { get; set; }
+
+    [JsonProperty("librarySectionTitle")]
+    public string LibrarySectionTitle { get; set; }
+
+    [JsonProperty("librarySectionID")]
+    public int LibrarySectionID { get; set; }
+
+    [JsonProperty("librarySectionKey")]
+    public string LibrarySectionKey { get; set; }
+
+    [JsonProperty("grandparentTitle")]
+    public string GrandparentTitle { get; set; }
+
+    [JsonProperty("parentTitle")]
+    public string ParentTitle { get; set; }
+
+    [JsonProperty("contentRating")]
+    public string ContentRating { get; set; }
+
+    [JsonProperty("summary")]
+    public string Summary { get; set; }
+
+    [JsonProperty("index")]
+    public int Index { get; set; }
+
+    [JsonProperty("parentIndex")]
+    public int ParentIndex { get; set; }
+
+    [JsonProperty("skipCount")]
+    public int SkipCount { get; set; }
+
+    [JsonProperty("year")]
+    public int Year { get; set; }
+
+    [JsonProperty("thumb")]
+    public string Thumb { get; set; }
+
+    [JsonProperty("art")]
+    public string Art { get; set; }
+
+    [JsonProperty("parentThumb")]
+    public string ParentThumb { get; set; }
+
+    [JsonProperty("grandparentThumb")]
+    public string GrandparentThumb { get; set; }
+
+    [JsonProperty("grandparentArt")]
+    public string GrandparentArt { get; set; }
+
+    [JsonProperty("grandparentTheme")]
+    public string GrandparentTheme { get; set; }
+
+    [JsonProperty("duration")]
+    public int Duration { get; set; }
+
+    [JsonProperty("originallyAvailableAt")]
+    public string OriginallyAvailableAt { get; set; }
+
+    [JsonProperty("addedAt")]
+    public int AddedAt { get; set; }
+
+    [JsonProperty("updatedAt")]
+    public int UpdatedAt { get; set; }
+
+    [JsonProperty("chapterSource")]
+    public string ChapterSource { get; set; }
+
+    [JsonProperty("Image")]
+    public List<Image> Image { get; set; }
+
+    [JsonProperty("UltraBlurColors")]
+    public UltraBlurColors UltraBlurColors { get; set; }
+
+    [JsonProperty("Guid")]
+    public List<Guid> Guid { get; set; }
+
+    [JsonProperty("Director")]
+    public List<Director> Director { get; set; }
+
+    [JsonProperty("Writer")]
+    public List<Writer> Writer { get; set; }
+
+    [JsonProperty("Role")]
+    public List<Role> Role { get; set; }
+
+    [JsonProperty("Producer")]
+    public List<Producer> Producer { get; set; }
+}
+
+public class Player
+{
+    [JsonProperty("local")]
+    public bool Local { get; set; }
+
+    [JsonProperty("publicAddress")]
+    public string PublicAddress { get; set; }
+
+    [JsonProperty("title")]
+    public string Title { get; set; }
+
+    [JsonProperty("uuid")]
+    public string Uuid { get; set; }
+}
+
+public class Producer
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("filter")]
+    public string Filter { get; set; }
+
+    [JsonProperty("tag")]
+    public string Tag { get; set; }
+
+    [JsonProperty("tagKey")]
+    public string TagKey { get; set; }
+
+    [JsonProperty("thumb")]
+    public string Thumb { get; set; }
+}
+
+public class Role
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("filter")]
+    public string Filter { get; set; }
+
+    [JsonProperty("tag")]
+    public string Tag { get; set; }
+
+    [JsonProperty("tagKey")]
+    public string TagKey { get; set; }
+
+    [JsonProperty("role")]
+    public string RoleName { get; set; }
+
+    [JsonProperty("thumb")]
+    public string Thumb { get; set; }
+}
+
+public class Server
+{
+    [JsonProperty("title")]
+    public string Title { get; set; }
+
+    [JsonProperty("uuid")]
+    public string Uuid { get; set; }
+}
+
+public class UltraBlurColors
+{
+    [JsonProperty("topLeft")]
+    public string TopLeft { get; set; }
+
+    [JsonProperty("topRight")]
+    public string TopRight { get; set; }
+
+    [JsonProperty("bottomRight")]
+    public string BottomRight { get; set; }
+
+    [JsonProperty("bottomLeft")]
+    public string BottomLeft { get; set; }
+}
+
+public class Writer
+{
+    [JsonProperty("id")]
+    public int Id { get; set; }
+
+    [JsonProperty("filter")]
+    public string Filter { get; set; }
+
+    [JsonProperty("tag")]
+    public string Tag { get; set; }
+
+    [JsonProperty("tagKey")]
+    public string TagKey { get; set; }
+
+    [JsonProperty("thumb")]
+    public string Thumb { get; set; }
 }
 
