8d3c14884ceb37e1408ec067a526b7deba06ed57
Milosz Dura <miloszdura@gmail.com>
2025-07-31 15:57:20 +0100

    adding watching now websockets (#87)

--- api/Trackster.Api/Features/Media/Types/WatchingEpisodeRecord.cs
+++ api/Trackster.Api/Features/Media/Types/WatchingEpisodeRecord.cs
@@ -4,6 +4,7 @@ namespace Trackster.Api.Features.Media.Types;
 
 public class WatchingEpisodeRecord
 {
+    public WatchingAction Action { get; set; }
     public EpisodeRecord Episode { get; set; }
     public DateTime StartedAt { get; set; }
     public int MillisecondsWatched { get; set; }
