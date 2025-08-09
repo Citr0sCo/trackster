453b18becca7220961e7f6ec8e7a5a1cfc168ea1
Milosz Dura <miloszdura@gmail.com>
2025-08-09 15:49:17 +0100

    adding debug steps (#130)

--- api/Trackster.Api/Features/Media/Importers/TmdbImporter/TmdbImportProvider.cs
+++ api/Trackster.Api/Features/Media/Importers/TmdbImporter/TmdbImportProvider.cs
@@ -14,140 +14,199 @@ public class TmdbImportProvider
     
     public async Task<TmdbMovieDetails> GetDetailsForMovie(string reference)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
-  
-            using(var response = await httpClient.GetAsync($"3/movie/{reference}"))
+            var baseAddress = new Uri("https://api.themoviedb.org/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
             {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var parsedData = JsonConvert.DeserializeObject<TmdbMovieDetails>(responseData);
-                return parsedData ?? new TmdbMovieDetails();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+
+                using (var response = await httpClient.GetAsync($"3/movie/{reference}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var parsedData = JsonConvert.DeserializeObject<TmdbMovieDetails>(responseData);
+                    return parsedData ?? new TmdbMovieDetails();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to get movie details. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+
+        return new TmdbMovieDetails();
     }
     
     public async Task<TmdbShowDetails> GetDetailsForShow(string reference)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try 
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
-  
-            using(var response = await httpClient.GetAsync($"3/tv/{reference}"))
+            var baseAddress = new Uri("https://api.themoviedb.org/");
+
+            using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
             {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var parsedData = JsonConvert.DeserializeObject<TmdbShowDetails>(responseData);
-                return parsedData ?? new TmdbShowDetails();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+      
+                using(var response = await httpClient.GetAsync($"3/tv/{reference}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var parsedData = JsonConvert.DeserializeObject<TmdbShowDetails>(responseData);
+                    return parsedData ?? new TmdbShowDetails();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to get show details. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+
+        return new TmdbShowDetails();
     }
     
     public async Task<TmdbEpisodeDetails> GetEpisodeDetails(string seriesId, int seasonNumber, int episodeNumber)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try 
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
-  
-            using(var response = await httpClient.GetAsync($"3/tv/{seriesId}/season/{seasonNumber}/episode/{episodeNumber}"))
+            var baseAddress = new Uri("https://api.themoviedb.org/");
+
+            using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
             {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var parsedData = JsonConvert.DeserializeObject<TmdbEpisodeDetails>(responseData);
-                return parsedData ?? new TmdbEpisodeDetails();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+      
+                using(var response = await httpClient.GetAsync($"3/tv/{seriesId}/season/{seasonNumber}/episode/{episodeNumber}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var parsedData = JsonConvert.DeserializeObject<TmdbEpisodeDetails>(responseData);
+                    return parsedData ?? new TmdbEpisodeDetails();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to get episode details. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+
+        return new TmdbEpisodeDetails();
     }
 
     public async Task<TmdbMovieSearchResults> FindMovieByTitleAndYear(string title, int year)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try 
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+            var baseAddress = new Uri("https://api.themoviedb.org/");
 
-            var queryParams = new Dictionary<string, string>
+            using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
             {
-                { "query", title }, 
-                { "year", year.ToString() },
-                { "include_adult", "false" },
-                { "language", "en-US" }
-            };
-            
-            var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
-            
-            using(var response = await httpClient.GetAsync($"3/search/movie?{flatQueryParams}"))
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var parsedData = JsonConvert.DeserializeObject<TmdbMovieSearchResults>(responseData);
-                return parsedData ?? new TmdbMovieSearchResults();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+
+                var queryParams = new Dictionary<string, string>
+                {
+                    { "query", title }, 
+                    { "year", year.ToString() },
+                    { "include_adult", "false" },
+                    { "language", "en-US" }
+                };
+                
+                var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+                
+                using(var response = await httpClient.GetAsync($"3/search/movie?{flatQueryParams}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var parsedData = JsonConvert.DeserializeObject<TmdbMovieSearchResults>(responseData);
+                    return parsedData ?? new TmdbMovieSearchResults();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to find movie by title and year. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+        
+        return new  TmdbMovieSearchResults();
     }
 
     public async Task<TmdbShowSearchResults> FindShowByTitleAndYear(string title, int year)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try 
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+            var baseAddress = new Uri("https://api.themoviedb.org/");
 
-            var queryParams = new Dictionary<string, string>
+            using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
             {
-                { "query", title }, 
-                { "year", year.ToString() },
-                { "include_adult", "false" },
-                { "language", "en-US" }
-            };
-            
-            var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
-            
-            using(var response = await httpClient.GetAsync($"3/search/tv?{flatQueryParams}"))
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+
+                var queryParams = new Dictionary<string, string>
+                {
+                    { "query", title }, 
+                    { "year", year.ToString() },
+                    { "include_adult", "false" },
+                    { "language", "en-US" }
+                };
                 
-                Console.WriteLine($"[DEBUG] - 1/1 - Received response from TMDB search for show {responseData}.");
+                var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
                 
-                var parsedData = JsonConvert.DeserializeObject<TmdbShowSearchResults>(responseData);
-                return parsedData ?? new TmdbShowSearchResults();
+                using(var response = await httpClient.GetAsync($"3/search/tv?{flatQueryParams}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    
+                    Console.WriteLine($"[DEBUG] - 1/1 - Received response from TMDB search for show {responseData}.");
+                    
+                    var parsedData = JsonConvert.DeserializeObject<TmdbShowSearchResults>(responseData);
+                    return parsedData ?? new TmdbShowSearchResults();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to find show by title and year. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+
+        return new TmdbShowSearchResults();
     }
 
     public async Task<TmdbSeasonSearchResults> GetDetailsForSeason(int showIdentifier, int seasonNumber)
     {
-        var baseAddress = new Uri("https://api.themoviedb.org/");
-
-        using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
+        try 
         {
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+            var baseAddress = new Uri("https://api.themoviedb.org/");
 
-            var queryParams = new Dictionary<string, string>
+            using (var httpClient = new HttpClient{ BaseAddress = baseAddress })
             {
-                { "language", "en-US" }
-            };
-            
-            var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
-            
-            using(var response = await httpClient.GetAsync($"3/tv/{showIdentifier}/season/{seasonNumber}?{flatQueryParams}"))
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var parsedData = JsonConvert.DeserializeObject<TmdbSeasonSearchResults>(responseData);
-                return parsedData ?? new TmdbSeasonSearchResults();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {_authToken}");
+
+                var queryParams = new Dictionary<string, string>
+                {
+                    { "language", "en-US" }
+                };
+                
+                var flatQueryParams = string.Join("&", queryParams.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+                
+                using(var response = await httpClient.GetAsync($"3/tv/{showIdentifier}/season/{seasonNumber}?{flatQueryParams}"))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var parsedData = JsonConvert.DeserializeObject<TmdbSeasonSearchResults>(responseData);
+                    return parsedData ?? new TmdbSeasonSearchResults();
+                }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[FATAL] - Failed to find details for season. Exception: {ex.Message}.");
+            Console.WriteLine(ex);
+        }
+
+        return new TmdbSeasonSearchResults();
     }
 }
