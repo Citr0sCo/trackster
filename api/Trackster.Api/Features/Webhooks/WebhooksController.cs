d0eebde17dfca49623fb84dea731a44d0ec2886e
Milosz Dura <miloszdura@gmail.com>
2025-09-12 18:17:37 +0100

    Update WebhooksController.cs

--- api/Trackster.Api/Features/Webhooks/WebhooksController.cs
+++ api/Trackster.Api/Features/Webhooks/WebhooksController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Trackster.Api.Features.Users;
 using Trackster.Api.Features.Webhooks.Types;
+using Trackster.Api.Attributes;
 
 namespace Trackster.Api.Features.Webhooks;
 
