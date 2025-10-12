e82ad4524fd7986520413156693b6296f2e4d806
Milosz Dura <miloszdura@gmail.com>
2025-10-12 10:13:30 +0100

    refactoring trakt auth (#272)

--- api/Trackster.Api/Features/Media/Importers/TmdbImporter/Types/TmdbCommonResponse.cs
+++ api/Trackster.Api/Features/Media/Importers/TmdbImporter/Types/TmdbCommonResponse.cs
@@ -5,11 +5,11 @@ namespace Trackster.Api.Features.Media.Importers.TmdbImporter.Types;
 public class TmdbCommonResponse
 {
     [JsonProperty("success")]
-    public bool IsSuccess { get; set; }
+    public bool? IsSuccess { get; set; }
 
     [JsonProperty("status_code")]
-    public int StatusCode { get; set; }
+    public int? StatusCode { get; set; }
 
     [JsonProperty("status_message")]
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 }
